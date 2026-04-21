18d9f4fa12ec60d5f4f868877390430000693005
LlanosNing <llanosning@gmail.com>
2026-04-21 13:49:10 +0200

    ajustes clase

--- Assets/Scripts/Shake.cs
+++ Assets/Scripts/Shake.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class Shake : MonoBehaviour
 {
-   
+    public float magnitude = 0.1f; 
+    public float speed = 1.0f; 
+
+    Vector3 initialPosition;
+
+    void Start()
+    {
+        initialPosition = transform.localPosition;
+    }
+
+    void Update()
+    {
+        Vector3 randomOffset = Random.insideUnitSphere * magnitude;
+
+        transform.localPosition = initialPosition + randomOffset;
+    }
 }
